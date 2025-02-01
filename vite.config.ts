36ae075d4623c4d0a45c3b140972b8f58fe3d535
Marissa Gvozdenovich <66572562+mgvoz@users.noreply.github.com>
2025-02-01 17:34:02 +0000

    replace base with app name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/personal-finance-app",
   preview: {
     allowedHosts: ["4f74lv-4173.csb.app", "4f74lv-5173.csb.app"],
   },
